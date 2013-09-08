48d0fdef64d2c2bbdc224e9939a5370f6dbea864
riverinyo <riverinyo@gmail.com>
2013-09-08 03:53:28 -0400

    test inserts get

--- api/parsecallback.php
+++ api/parsecallback.php
@@ -2,24 +2,44 @@
 
 include("mheader.php");
 
-$json = (isset($_REQUEST['data']) && !empty($_REQUEST['data'])) ? json_decode($_REQUEST['data'], true) : array(); 
-error_log(serialize($json));
-echo serialize($json);
+// $json = (isset($_REQUEST['data']) && !empty($_REQUEST['data'])) ? json_decode($_REQUEST['data'], true) : array(); 
+// error_log(serialize($json));
+// echo serialize($json);
+
+// $cons = new Consumption($connection);
+
+// foreach($json as $key => $value) {
+// // 	$consumption = array(
+// // 			'Start' => '2013-01-01 00:00:00',
+// // 			'Duration' => 3600,
+// // 			'Cost' => 1,
+// // 			'Value' => 1);
+// 	$consumption = array(
+// 			'Start' => $json['time'],
+// 			'Duration' => 3600,
+// 			'Cost' => $json['cost'],
+// 			'Value' => $json['value']);
+// 	$cons->insertConsumption($consumption);
+// }
+
+$cost = (isset($_REQUEST['cost']) && !empty($_REQUEST['cost'])) ? $_REQUEST['cost'] : 0;
+$value = (isset($_REQUEST['value']) && !empty($_REQUEST['value'])) ? $_REQUEST['value'] : 0;
+$start = (isset($_REQUEST['time']) && !empty($_REQUEST['time'])) ? new DateTime($_REQUEST['time']) : new DateTime();
 
 $cons = new Consumption($connection);
 
-foreach($json as $key => $value) {
 // 	$consumption = array(
 // 			'Start' => '2013-01-01 00:00:00',
 // 			'Duration' => 3600,
 // 			'Cost' => 1,
 // 			'Value' => 1);
-	$consumption = array(
-			'Start' => $json['time'],
-			'Duration' => 3600,
-			'Cost' => $json['cost'],
-			'Value' => $json['value']);
-	$cons->insertConsumption($consumption);
-}
+$consumption = array(
+		'Start' => $start->format('Y-m-d H:i:s'),
+		'Duration' => 3600,
+		'Cost' => $cost,
+		'Value' => $value);
+if(!$cons->insertConsumption($consumption))
+	error_log("Insert Fail");
+
 
 ?>
